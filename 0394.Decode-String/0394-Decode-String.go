434ec2619aca8211e3dcd34f57bbce9122ae7ee2
spotify2junkie <lsemarkluo@gmail.com>
2019-12-17 23:48:19 +0800

    辅助栈解题

---
+++ 0394.Decode-String/0394-Decode-String.go
@@ -0,0 +1,37 @@
+type item struct {
+    prestr string
+    loopNum int 
+}
+
+//实现判断第一个为数字功能
+func helper(s string) string {
+    var res string // 移动的单位
+    var loopNum int 
+    var stack[]item // 通过item stack 实现
+    for step := 0 ; step < len(s); step++{
+        v := s[step]
+        if v >= '0' && v <= '9' {
+            loopNum = loopNum*10 + int(v-'0')
+        } else if v == '[' {
+            stack = append(stack,item{prestr: res, loopNum: loopNum})
+            res = ""      //dynamic programmin
+            loopNum = 0
+        } else if v == ']' {
+            item := stack[len(stack)-1] // pop
+            for i := 0; i < item.loopNum; i++ {
+                item.prestr += res
+            }
+            res = item.prestr
+            stack = stack[:len(stack)-1] // 剪栈 没匹配的还留着比如第一个"["
+        } else {
+            res += string(v)
+        }
+    }
+    return res 
+}
+
+func decodeString(s string) string {
+    return helper(s)
+}
+
+
