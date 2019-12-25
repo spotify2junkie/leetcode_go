2b1968dc970501a61b0ba2219edacbf2c6f7b2d3
spotify2junkie <lsemarkluo@gmail.com>
2019-12-25 23:16:25 +0800

    Formalize All Folders

---
+++ 0206-Reverse-Linked-List/solution.go
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode
+    for head != nil {
+        temp := head.Next  // 暂存
+        head.Next = prev // 指向之前的节点
+        prev = head  // 存入之前的节点
+        head = temp  // 找到下一个开始的地方
+    }
+    return prev
+}
