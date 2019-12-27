f4c92d0934314e3d88b380d97c4cfdef06a3d817
spotify2junkie <lsemarkluo@gmail.com>
2019-12-27 10:16:23 +0800

    Linked List

---
+++ 0142-Linked-Cycle-II/solution.go
@@ -0,0 +1,14 @@
+// linked list , using hash table 
+
+func detectCycle(head *ListNode) *ListNode {
+    cur := head  // 为什么每次都要存一下 可以不存吗 
+    checked := make(map[*ListNode]int)  // why use map[*ListNode] 
+    for cur != nil {
+        if _,ok := checked[cur]; ok { 
+            return cur  
+        }
+        checked[cur] = 1
+        cur = cur.Next
+    }
+    return nil 
+}
---
+++ 0234-Palindrome-Linked-List/solution.go
@@ -0,0 +1,17 @@
+func isPalindrome(head *ListNode) bool {
+    res := []int{}
+    for head != nil {
+        res = append(res,head.Val)
+        head = head.Next
+    }
+    // 按照规则对比值
+	l, r := 0, len(res)-1
+	for l < r {
+		if res[l] != res[r] {
+			return false
+		} 
+		l++ //这个可以
+		r-- // 不用来回
+	}
+	return true
+}
