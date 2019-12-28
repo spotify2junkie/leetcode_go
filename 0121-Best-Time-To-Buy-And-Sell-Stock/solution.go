7a3d53c498ebc327f8349ac1152d55d48533f9e3
spotify2junkie <lsemarkluo@gmail.com>
2019-12-28 18:01:27 +0800

    Dynamic Programming

---
+++ 0121-Best-Time-To-Buy-And-Sell-Stock/solution.go
@@ -0,0 +1,14 @@
+// dynamic programming 
+func maxProfit(prices []int) int {
+    max := 0
+    temp := 0
+    for i := 1; i < len(prices);i ++ {
+        temp += prices[i]-prices[i-1]
+        if temp > max {
+            max = temp
+        } else if temp < 0 {
+            temp = 0 
+        }
+    }
+    return max 
+}
--- 0279-Perfect-Square/solution.go
+++ 0279-Perfect-Square/solution.go
@@ -1,21 +1,28 @@
 // dynamic programming , DFS 
-
 func numSquares(n int) int {
-	if n < 0 {
-		return 0
-	}
+    if n >= 0 && n < 2 {
+        return n 
+    }
+    dp := make([]int,n+1)
+    perfects := []int{}
+    // gather all squares smaller than n 
+    for i:=1 ; i*i <= n;i++ {
+        perfects = append(perfects,i*i)
+    }
 
-	if n <= 1 {
-		return 1
-	}
+    for i:=1;i < n+1;i++ {
+        dp[i] = i 
+        for _,s :=range perfects {
+            if i-s >= 0 {
+                dp[i] = min(dp[i],dp[i-s]+1) // either itself or 12-9 = 3  , dp[3] + 1
+            }
+        }
+    }
+    return dp[n]
+}
 
-	dp := make([]int,n+1)
-	for i:=1; i < n +1 ; i ++ {
-		dp[i] = i 
-		for j :=1, i - j*j >= 0; j++ {
-			dp[i] = min(dp[i],dp[i-j*j]+1)  
-		}
-	}
-
-	return dp[n]
+func min(x,y int) int {
+    if x < y {
+        return x
+    } else { return y }
 }
