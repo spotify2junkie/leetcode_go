47d88d38993735064f87fb88d5033bef3d9a21cf
spotify2junkie <lsemarkluo@gmail.com>
2019-12-29 18:02:21 +0800

    Dynamic Programming

---
+++ 0322-Coin-Change/README.md
@@ -0,0 +1,2 @@
+# Notes 
+- 动态规划 (dp[n] = min(dp[n],dp[n-coin_value]))
---
+++ 0322-Coin-Change/solution.go
@@ -0,0 +1,25 @@
+func coinChange(coins []int, amount int) int {
+    dp := make([]int,amount+1)
+    dp[0] = 0 
+    for i:=1;i < len(dp);i++ {
+        dp[i] = amount+100
+    }
+    for i := 1; i <= amount; i++ {
+        for _,coin := range coins {
+            if coin <= i {
+                dp[i] = min(dp[i],dp[i-coin]+1)  // 自己本身（初始的自己（被设定）或者迭代的自己最小）或者 coin的最小值
+            }
+        }
+    }
+    if dp[amount] == amount+100 {
+        return -1
+    } else {
+        return dp[amount]
+    }
+}
+
+func min (x,y int) int {
+    if x > y {
+        return y
+    } else { return x}
+}
