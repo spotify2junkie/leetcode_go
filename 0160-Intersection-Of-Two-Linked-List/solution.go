80ef8c88e277ba5270494672250a0984315a09ee
spotify2junkie <lsemarkluo@gmail.com>
2020-01-09 23:23:24 +0800

    Linked List

---
+++ 0160-Intersection-Of-Two-Linked-List/solution.go
@@ -0,0 +1,21 @@
+package main
+
+
+	 getIntersecti onNode(headA, headB *ListNode) *ListNode {
+	a, b := headA,headB
+		 != b {
+			 != nil {
+		    a = a.Next
+			se {
+		    a = headB
+         }
+		
+			 != nil {
+		    b = b.Next
+			se {
+		    b = headA
+	     }
+	}
+    return b
+
+}
