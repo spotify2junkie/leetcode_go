39ceda485a1748dcf02f23821e935120a4f0393a
spotify2junkie <lsemarkluo@gmail.com>
2020-01-13 22:31:29 +0800

    Slice

---
+++ 0055-Jump-games/solution.go
@@ -0,0 +1,25 @@
+// greedy algorithm
+
+func canJump(nums []int) bool {
+    for i:=len(nums)-2;i>=0;i-- { //nnn
+        // find 0
+        if nums[i] != 0 {
+            continue 
+        }
+        j := i - 1
+		for ; j >= 0; j-- {
+			if i-j < nums[j] {
+				// 在 j 号位置上，可以跨过 0 元素
+				i = j
+				break
+			}
+		}
+
+		if j == -1 {
+			// 在 0 元素之前，没有位置可以跨过 0
+			return false
+		}
+	}
+
+	return true
+}
