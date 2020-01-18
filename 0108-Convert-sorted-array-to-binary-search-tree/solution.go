2c22941e0f3ea36d91b482b0299459844907bd3e
spotify2junkie <lsemarkluo@gmail.com>
2020-01-18 11:29:41 +0800

    Binary Search Tree

---
+++ 0108-Convert-sorted-array-to-binary-search-tree/solution.go
@@ -0,0 +1,8 @@
+//nnn Binary Search Tree
+func sortedArrayToBST(nums []int) *TreeNode {
+    if len(nums) == 0 {
+        return nil
+    }
+    return &TreeNode{Val: nums[len(nums)/2], Left: sortedArrayToBST(nums[:len(nums)/2]),
+                    Right: sortedArrayToBST(nums[len(nums)/2 + 1:])}
+}
