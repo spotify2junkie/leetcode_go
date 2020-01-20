ef6be4b149efaa984bd43be874f3b9ac0c7c645b
spotify2junkie <lsemarkluo@gmail.com>
2020-01-20 21:34:14 +0800

    Array

---
+++ 0026-Remove-duplicates-from-sorted-array/solution.go
@@ -0,0 +1,12 @@
+//nnn array
+func removeDuplicates(nums []int) int {
+    left,right,size := 0,1,len(nums)
+    for ; right < size;right ++ {
+        if nums[left] == nums[right] {
+            continue
+        }
+        left++
+        nums[left],nums[right] = nums[right],nums[left]
+    }
+    return left+1
+}
