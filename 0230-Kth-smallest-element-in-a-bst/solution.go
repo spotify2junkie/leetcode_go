cdeb2864c9cf0c7329d0c6ca8b0c6a4fdf9fdb4e
spotify2junkie <lsemarkluo@gmail.com>
2020-01-26 15:29:07 +0800

    Inorder Travesal

---
+++ 0230-Kth-smallest-element-in-a-bst/solution.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func inorderTraversal(root *TreeNode) []int {
+    node_val := []int{}
+    if root != nil {
+        node_val = append(node_val,inorderTraversal(root.Left)...)
+        node_val = append(node_val, root.Val)
+        node_val = append(node_val,inorderTraversal(root.Right)...)
+    }
+    return node_val
+}
+
+func kthSmallest(root *TreeNode, k int) int {
+    res := inorderTraversal(root)
+    return res[k-1]
+}
