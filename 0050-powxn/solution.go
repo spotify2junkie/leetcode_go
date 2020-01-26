607573cd9e376e15050cc1fdb3235201b5061af7
spotify2junkie <lsemarkluo@gmail.com>
2020-01-26 12:30:16 +0800

    Binary Search

---
+++ 0050-powxn/solution.go
@@ -0,0 +1,20 @@
+func myPow(x float64, n int) float64 {
+    if x < 0 {
+        return 1.0 / power(x,-n)
+    }
+    return pow(x,n)
+}
+
+func pow(x float64, n int) float64 {
+    if x == 0 {
+        return 0 
+    }
+    if n == 0 {
+        return 1 
+    }
+    res := pow(x,n>>1)
+    if n&1 == 0 {  //nnn 可以被整除
+        return res*res
+    }
+    return res*res*x
+}
