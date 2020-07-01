b5994f7121127d4fbad10b5946939cda2c1dc2d4
spotify2junkie <lsemarkluo@gmail.com>
2020-07-02 01:17:11 +0800

    add

---
+++ 0033-Search-in-rotated-sorted-arra/solution_second.go
@@ -0,0 +1,34 @@
+func search(nums []int, target int) int {
+    rotated := indexOfMin(nums)
+    size := len(nums)
+    start, end := 0, len(nums)-1
+    for start <= end {
+        mid := (start+end) >>1
+        originalMid := (mid+rotated) % size 
+        if nums[originalMid] < target {
+            start = mid
+        } else if nums[originalMid] > target {
+            end = mid
+        } else {
+            return originalMid
+        }
+    }
+    return -1
+
+}
+
+func indexOfMin(nums []int) int {
+    size := len(nums)
+    left, right := 0, size-1
+    for left < right {
+        mid := (left + right) / 2
+        if nums[right] < nums[mid] {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+    return left
+}
+
+
