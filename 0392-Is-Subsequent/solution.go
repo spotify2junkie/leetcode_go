b25751a781dc27535a6dab48c7a957214f63977e
spotify2junkie <lsemarkluo@gmail.com>
2020-09-07 22:46:30 +0800

    add

---
+++ 0392-Is-Subsequent/solution.go
@@ -0,0 +1,10 @@
+// 贪心算法
+func isSubsequence(s string, t string) bool {
+    for len(s) > 0 && len(t) > 0 {
+        if s[0] == t[0] {
+            s = s[1:]
+        }
+        t =t[1:]
+    }
+    return len(s)==0
+}
