b357e34cc13730e77b4848366164f77a81637c14
spotify2junkie <lsemarkluo@gmail.com>
2020-09-21 13:44:10 +0800

    update

--- 0394-Decode-String/solution.go
+++ 0394-Decode-String/solution.go
@@ -1,44 +1,30 @@
-/*
-this one is not original work 
-*/
-//nnn array slice 
-
-
+func decodeString(s string) string {
+	var res string
+	var loopNum int
+	stack := []item{}
+	for i := 0; i < len(s); i++ {
+		v := s[i]
+		if v >= '0' && v <= '9' {
+			loopNum = loopNum*10 + int(v-'0')
+		} else if v == '[' {
+			stack = append(stack, item{preStr: res, loopNum: loopNum})
+			res = ""
+			loopNum = 0
+		} else if v == ']' {
+			item := stack[len(stack)-1]
+			for j := 0; j < item.loopNum; j++ {
+				item.preStr += res
+			}
+			res = item.preStr
+			stack = stack[:len(stack)-1]
+		} else {
+			res += string(v)
+		}
+	}
+	return res
+}
 
 type item struct {
-    prestr string
-    loopNum int 
+	preStr  string
+	loopNum int
 }
-
-//实现判断第一个为数字功能
-func helper(s string) string {
-    var res string // 移动的单位
-    var loopNum int 
-        // 通过item stack 实现
-    for step := 0 ; step < len(s); step++{
-        v := s[step]
-        if v >= '0' && v <= '9' {
-            loopNum = loopNum*10 + int(v-'0')
-        } else if v == '[' {
-            stack = append(stack,item{prestr: res, loopNum: loopNum})
-            res = ""      //dynamic programmin
-            loopNum = 0
-        } else if v == ']' {
-            item := stack[len(stack)-1] // pop
-            for i := 0; i < item.loopNum; i++ {
-                item.prestr += res
-            }
-            res = item.prestr
-            stack = stack[:len(stack)-1] // 剪栈 没匹配的还留着比如第一个"["
-        } else {
-            res += string(v)
-        }
-    }
-    return res 
-}
-
-func decodeString(s string) string {
-    return helper(s)
-}
-
-
