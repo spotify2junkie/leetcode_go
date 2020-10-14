a64fdd97d0c1ecb1679c828ece798c61b4597d34
spotify2junkie <lsemarkluo@gmail.com>
2020-10-14 09:16:18 +0800

    update dp

--- 0139-Word-Break/solution.go
+++ 0139-Word-Break/solution.go
@@ -1,15 +1,16 @@
-//nnn dp 
+//nnn dp
 func wordBreak(s string, wordDict []string) bool {
-    dp := make([]bool,len(s))
-    for i:=0; i < len(s);i++ {
-        for _,key := range wordDict {
-            if i < len(key) -1 {
-                continue // skip part 
-            }
-            if string(s[i-len(key)+1:i+1]) == key && (i == len(key) - 1 || dp[i-len(key)])  {
-                dp[i] = true 
-            }
-        }
-    }
-    return dp[len(s)-1]
+	// 运用dp , 是因为dp 的连接作用
+	dp := make([]bool, len(s))
+	for i := 0; i < len(s); i++ {
+		for _, key := range wordDict {
+			if i < len(key)-1 {
+				continue // skip part
+			}
+			if string(s[i-len(key)+1:i+1]) == key && (i == len(key)-1 || dp[i-len(key)]) {
+				dp[i] = true
+			}
+		}
+	}
+	return dp[len(s)-1]
 }
--- 0279-Perfect-Square/solution.go
+++ 0279-Perfect-Square/solution.go
@@ -1,28 +1,30 @@
-// dp  dynamic programming , DFS 
+// dp  dynamic programming , DFS
 func numSquares(n int) int {
-    if n >= 0 && n < 2 {
-        return n 
-    }
-    dp := make([]int,n+1)
-    perfects := []int{}
-    // gather all squares smaller than n 
-    for i:=1 ; i*i <= n;i++ {
-        perfects = append(perfects,i*i)
-    }
+	if n >= 0 && n < 2 {
+		return n
+	}
+	dp := make([]int, n+1)
+	perfects := []int{}
+	// gather all squares smaller than n
+	for i := 1; i*i <= n; i++ {
+		perfects = append(perfects, i*i)
+	}
 
-    for i:=1;i < n+1;i++ {
-        dp[i] = i //因为每个最长的状态是1的累加 
-        for _,s :=range perfects {
-            if i-s >= 0 {
-                dp[i] = min(dp[i],dp[i-s]+1) // either itself or 12-9 = 3  , dp[3] + 1
-            }
-        }
-    }
-    return dp[n]
+	for i := 1; i < n+1; i++ {
+		dp[i] = i //因为每个最长的状态是1的累加 // nice
+		for _, s := range perfects {
+			if i-s >= 0 {
+				dp[i] = min(dp[i], dp[i-s]+1) // either itself or 12-9 = 3  , dp[3] + 1
+			}
+		}
+	}
+	return dp[n]
 }
 
-func min(x,y int) int {
-    if x < y {
-        return x
-    } else { return y }
+func min(x, y int) int {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
 }
