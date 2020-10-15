8189bf0e988a52b9cb53a092c5396e57aafa6c86
spotify2junkie <lsemarkluo@gmail.com>
2020-10-15 23:20:51 +0800

    update recursion

--- 0050-powxn/solution.go
+++ 0050-powxn/solution.go
@@ -1,21 +1,20 @@
-//nnn math using recursion 
 func myPow(x float64, n int) float64 {
-    if x < 0 {
-        return 1.0 / power(x,-n)
-    }
-    return pow(x,n)
+	if n < 0 {
+		return 1 / pow(x, -n)
+	}
+	return pow(x, n)
 }
 
 func pow(x float64, n int) float64 {
-    if x == 0 {
-        return 0 
-    }
-    if n == 0 {
-        return 1 
-    }
-res := pow(x,n>>1)
-         if n&1 == 0 {  //nnn 可以被整除
-             return res*res
-         }
-     return res*res*x
+	if x == 0 {
+		return 0
+	}
+	if n == 0 {
+		return 1
+	}
+	res := pow(x, n/2)
+	if n%2 == 0 {
+		return res * res
+	}
+	return res * res * x
 }
