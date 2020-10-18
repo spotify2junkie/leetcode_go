f951ab0033a370e43508b01d0fd8e1f9894ce9a3
spotify2junkie <lsemarkluo@gmail.com>
2020-10-19 00:27:21 +0800

    update

--- 0039-Combination-sum/solution.go
+++ 0039-Combination-sum/solution.go
@@ -1,23 +1,36 @@
-// backtrack 
-func combinationSum(candidates []int, target int) [][]int {
-    //nnn backtrack 可以实现
-    result := make([][]int,0)
-    backtrack([]int{},candidates,target,0,&result)
-    return result
+// backtrack
+import "sort"
+
+func combinationSum(c []int, t int) [][]int {
+	res := [][]int{}
+	if len(c) == 0 {
+		return res
+	}
+	p := []int{}
+	sort.Ints(c)
+	genPermute(c, t, p, 0, &res)
+	return res
 }
 
-func backtrack(subset, candidates []int, target,sum int, res *[][]int) {
-    if sum == target {
-        *res = append(*res,subset)
-        return 
-    } else if sum > target {
-        return  //nnn you only break look so here return 
-    }
-    for i := range candidates { //nnn  this 结构 
-        subsetCopy := make([]int, 0, len(subset)+1)
-        subsetCopy = append(subsetCopy,subset...)
-        backtrack(append(subsetCopy,candidates[i]),candidates[i:],target,sum+candidates[i],res) 
-    }
-}
+func genPermute(c []int, t int, p []int, index int, res *[][]int) {
+	if t <= 0 {
+		if t < 0 {
+			return
+		}
+		temp := make([]int, len(p))
+		copy(temp, p)
+		*res = append(*res, temp)
+		return
+	}
 
+	for i := index; i < len(c); i++ {
+		if c[i] > t { // 这里可以剪枝优化
+			break
+		}
+		p = append(p, c[i])
+		genPermute(c, t-c[i], p, i, res)
+		p = p[:len(p)-1]
+	}
+	return
+}
 
