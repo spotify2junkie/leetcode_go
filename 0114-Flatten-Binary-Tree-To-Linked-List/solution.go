36228d6faf35ecf5f8ceba6d8e39e93e5da94e0d
spotify2junkie <lsemarkluo@gmail.com>
2020-10-19 22:02:57 +0800

    update

--- 0114-Flatten-Binary-Tree-To-Linked-List/solution.go
+++ 0114-Flatten-Binary-Tree-To-Linked-List/solution.go
@@ -1,4 +1,4 @@
-//nnn linked list 
+//nnn linked list
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -7,17 +7,17 @@
  *     Right *TreeNode
  * }
  */
- func flatten(root *TreeNode)  {
-    if root == nil {
-        return 
-    }
-    flatten(root.Right)
-    flatten(root.Left)
-    // for r 
-    tmp := root.Right // 暂存
-    root.Right, root.Left = root.Left, nil
-    for root.Right != nil {  // for 直到6 
-        root = root.Right
-    }
-    root.Right = tmp // 连上
+func flatten(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	flatten(root.Right)
+	flatten(root.Left)
+	// for r
+	tmp := root.Right                      // 暂存
+	root.Right, root.Left = root.Left, nil // 空头
+	for root.Right != nil {                // for 直到6
+		root = root.Right
+	}
+	root.Right = tmp // 连上
 }
