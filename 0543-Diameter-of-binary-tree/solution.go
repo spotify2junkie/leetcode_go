a9d20d5b2258f3dfbfa2c26c823b4d7f92ac4810
spotify2junkie <lsemarkluo@gmail.com>
2020-10-24 16:48:04 +0800

    update

--- 0543-Diameter-of-binary-tree/solution.go
+++ 0543-Diameter-of-binary-tree/solution.go
@@ -1,24 +1,25 @@
 // recursion binary tree
-var max_depth int  // 定义一个全局变量比较max_depth 
+var max_depth int // 定义一个全局变量比较max_depth
 func diameterOfBinaryTree(root *TreeNode) int {
-    if root == nil {
-        return 0 
-    }
-    depth(root)
-    return max_depth
+	if root == nil {
+		return 0
+	}
+	depth(root)
+	return max_depth
 }
 
-
 func depth(node *TreeNode) int {
-    if node == nil {
-        return 0
-    }
-    max_depth = max(max_depth,depth(node.Right)+depth(node.Left))
-    return max(depth(node.Right),depth(node.Left))+1
+	if node == nil {
+		return 0
+	}
+	max_depth = max(max_depth, depth(node.Right)+depth(node.Left))
+	return max(depth(node.Right), depth(node.Left)) + 1
 }
 
 func max(a, b int) int {
-    if a >= b {
-        return a 
-    } else { return b }
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
 }
