a3a0d97e1f02486bac6be3f25c1b4732135d135e
spotify2junkie <lsemarkluo@gmail.com>
2020-10-24 20:56:50 +0800

    update

--- 0538-Convert-bst-to-greater-tree/solution.go
+++ 0538-Convert-bst-to-greater-tree/solution.go
@@ -1,22 +1,21 @@
-// Traverse Pointer  DFS 
+// Traverse Pointer  DFS
 
-ffunc convertBST(root *TreeNode) *TreeNode {
-    if root == nil {
-        return nil 
-    }
-    sum := 0 
-    getAdd(root,&sum) //取地址
-    return root
+func convertBST(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	sum := 0
+	helper(root, &sum)
+	return root
 }
 
-func getAdd(root *TreeNode, sum *int) *TreeNode { // int类型
-    if root == nil {
-        return nil 
-    }
-    // right 
-    getAdd(root.Right,sum)  // 先后顺序
-    *sum += root.Val
-    root.Val = *sum
-    getAdd(root.Left, sum)
-    return root
+func helper(r *TreeNode, sum *int) {
+	if r == nil {
+		return
+	}
+	helper(r.Right, sum)
+	*sum = *sum + r.Val
+	r.Val = *sum
+	helper(r.Left, sum)
+	return
 }
