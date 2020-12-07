08d62d205e78eec1aef8e4c9b085b0b8cdf66b0c
spotify2junkie <lsemarkluo@gmail.com>
2020-12-07 22:30:39 +0800

    update 模板

--- 0003-Longest-SubString-Without-Repeating-Characters/solution.go
+++ 0003-Longest-SubString-Without-Repeating-Characters/solution.go
@@ -1,56 +1,25 @@
-//nnn slice, array,sliding window
 func lengthOfLongestSubstring(s string) int {
-    if len(s) == 0 {
-        return 0
-    }
-    var freq [256]int
-    left, right,result := 0,0,0
-    for left < len(s) {
-        if right < len(s) && freq[s[right]-'a'] == 0 {
-            freq[s[right]-'a']++
-            right++
-        } else {
-            freq[s[left]-'a']-- // 经典
-            left++
-        }
-        result = max(result, right-left)
-    }
-    return result
+	l, r := 0, 0
+	count := make(map[byte]int)
+	res := 0
+	for r < len(s) {
+		c := s[r]
+		count[c]++
+		r++
+		for count[c] > 1 { //模板
+			d := s[l]
+			count[d]--
+			l++
+		}
+		res = max(res, r-l)
+	}
+	return res
 }
 
-func max(i, j int) int {
-    if i > j {
-        return i 
-    } else {
-        return j 
-    }
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
 }
-
-// another solution 
-// // another solution
-// func lengthOfLongestSubstring(s string) int {
-//     if len(s) == 0 {
-//         return 0
-//     }
-//     var freq [256]int
-//     left, right,result := 0,-1,0
-//     for left < len(s) {
-//         if right+1 < len(s) && freq[s[right+1]-'a'] == 0 {
-//             freq[s[right+1]-'a']++
-//             right++
-//         } else {
-//             freq[s[left]-'a']-- // 经典
-//             left++
-//         }
-//         result = max(result, right-left+1)
-//     }
-//     return result
-// }
-
-// func max(i, j int) int {
-//     if i > j {
-//         return i 
-//     } else {
-//         return j 
-//     }
-// }
---
+++ 3.longest-substring-without-repeating-characters.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=3 lang=golang
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+func lengthOfLongestSubstring(s string) int {
+	l, r := 0, 0
+	count := make(map[byte]int)
+	for r < len(s) {
+		c := r
+		count[c]++
+		right++
+		if _, ok := count[c]; ok >= 1 {
+			d := s[left]
+			count[d]--
+			left++
+		}
+		res = max(res, right-left)
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {return a}
+	else {return b}
+}
+// @lc code=end
+
