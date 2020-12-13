a07cde7df65dab20994369a08ccf3f33f3759178
spotify2junkie <lsemarkluo@gmail.com>
2020-12-14 01:06:12 +0800

    update

---
+++ 0092-rever-linked-list-ii/solution.go
@@ -0,0 +1,22 @@
+// recursion
+
+var succssor *ListNode
+
+func reverseBetween(head *ListNode, m int, n int) *ListNode {
+	if m == 1 {
+		return reverseRange(head, n)
+	}
+	head.Next = reverseBetween(head.Next, m-1, n-1)
+	return head
+}
+
+func reverseRange(head *ListNode, n int) *ListNode {
+	if n == 1 {
+		succssor = head.Next
+		return head
+	}
+	last := reverseRange(head.Next, n-1)
+	head.Next.Next = head
+	head.Next = succssor
+	return last
+}
