9d833bfe30f2374020346a0624c374611cc6e421
spotify2junkie <lsemarkluo@gmail.com>
2020-12-18 13:15:57 +0800

    update

--- 0234-Palindrome-Linked-List/solution.go
+++ 0234-Palindrome-Linked-List/solution.go
@@ -1,16 +1,16 @@
-//nnn linked list 
+//nnn linked list
 func isPalindrome(head *ListNode) bool {
-    res := []int{}
-    for head != nil {
-        res = append(res,head.Val)
-        head = head.Next
-    }
-    // 按照规则对比值
+	res := []int{}
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+	// 按照规则对比值
 	l, r := 0, len(res)-1
 	for l < r {
 		if res[l] != res[r] {
 			return false
-		} 
+		}
 		l++ //这个可以
 		r-- // 不用来回
 	}
