44e8e5b54b99160aac81420be8603e81d83a9d9c
spotify2junkie <lsemarkluo@gmail.com>
2020-12-18 13:29:09 +0800

    update

--- 0242-Valid-anagrams/solution.go
+++ 0242-Valid-anagrams/solution.go
@@ -1,14 +1,14 @@
 func isAnagram(s string, t string) bool {
-    if len(s) != len(t) {
-        return false 
-    }
-    sm := make(map[byte]int)
-    for i := range s {
-        sm[s[i]]++
-        sm[t[i]]--
-    }
+	if len(s) != len(t) {
+		return false
+	}
+	sm := make(map[byte]int)
+	for i := range s {
+		sm[s[i]]++ //巧妙， 两个一起
+		sm[t[i]]--
+	}
 
-    for _, n := range sm {
+	for _, n := range sm {
 		if n != 0 {
 			return false
 		}
