cab0742b75404264b16268d0caa9040ff42b9df3
spotify2junkie <lsemarkluo@gmail.com>
2021-03-07 20:07:43 +0800

    修正认知

--- 0086-Partition-List/solution.go
+++ 0086-Partition-List/solution.go
@@ -7,19 +7,20 @@
  * }
  */
 func partition(head *ListNode, x int) *ListNode {
-	beforeHead := &ListNode{Val: 0, Next: nil} // 取地址
-	afterHead := &ListNode{Val: 0, Next: nil}  // 取地址
-	for head != nil {
+	before, after := &ListNode{}, &ListNode{}
+	start, mid := before, after
+	for head != nil { // 修正认知
 		if head.Val < x {
-			a.Next = head
-			a = a.Next
+			before.Next = &ListNode{Val: head.Val}
+			before = before.Next
 		} else {
-			b.Next = head
-			b = b.Next
+			after.Next = &ListNode{Val: head.Val}
+			after = after.Next
 		}
 		head = head.Next
 	}
-	b.Next = nil // 定义空
-	a.Next = afterHead.Next
-	return beforeHead.Next
+	before.Next = mid.Next
+	after.Next = nil
+	return start.Next
 }
+
