7059e275f0aee8e821082802a09e96086c032d71
spotify2junkie <lsemarkluo@gmail.com>
2021-03-07 19:52:43 +0800

    修正认知

--- 0141-Linked-List-Cycle/solution.go
+++ 0141-Linked-List-Cycle/solution.go
@@ -1,33 +1,11 @@
-// hash table,linked list , two pointes
-func hasCycle(head *ListNode) bool {
-	hash := make(map[*ListNode]int) // 这个添加方式
-	for head != nil {
-		if _, ok := hash[head]; ok { // 查看是否存在
-			return true
-		}
-		hash[head] = head.val // store value
-		head = head.Next
-	}
-	return false
-
-}
-
-// second solution, use two pointers
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func hasCycle(head *ListNode) bool {
 	if head == nil || head.Next == nil || head.Next.Next == nil {
 		return false
 	}
-	fast, slow := &ListNode{}, &ListNode{}
-	fast = head.Next.Next
-	slow = head.Next
-	for fast != nil && fast.Next != nil { // 只需关注下面用到的fast.Next 是否为空
+	// fast, slow := &ListNode{}, &ListNode{}
+
+	fast, slow := head.Next.Next, head.Next
+	for fast.Next != nil && fast.Next.Next != nil {
 		if fast.Val == slow.Val {
 			return true
 		}
@@ -36,5 +14,3 @@ func hasCycle(head *ListNode) bool {
 	}
 	return false
 }
-
-
