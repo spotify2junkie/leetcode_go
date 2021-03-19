8aad39b988e9e7d1ce329d3cb7203d3babb88add
spotify2junkie <lsemarkluo@gmail.com>
2021-03-19 18:56:37 +0800

    udpate

--- 0279-Perfect-Square/solution.go
+++ 0279-Perfect-Square/solution.go
@@ -1,30 +1,26 @@
-// dp  dynamic programming , DFS
+// dp
+// redo
 func numSquares(n int) int {
-	if n >= 0 && n < 2 {
-		return n
+	squares := []int{}
+	for i := 1; i*i <= n; i++ {
+		squares = append(squares, i*i)
 	}
 	dp := make([]int, n+1)
-	perfects := []int{}
-	// gather all squares smaller than n
-	for i := 1; i*i <= n; i++ {
-		perfects = append(perfects, i*i)
-	}
-
-	for i := 1; i < n+1; i++ {
-		dp[i] = i //因为每个最长的状态是1的累加 // nice
-		for _, s := range perfects {
-			if i-s >= 0 {
-				dp[i] = min(dp[i], dp[i-s]+1) // either itself or 12-9 = 3  , dp[3] + 1
+	for i := 1; i <= n; i++ {
+		dp[i] = i
+		for _, element := range squares {
+			if element <= i {
+				dp[i] = min(dp[i], dp[i-element]+1)
 			}
 		}
 	}
 	return dp[n]
 }
 
-func min(x, y int) int {
-	if x < y {
-		return x
+func min(i, j int) int {
+	if i > j {
+		return j
 	} else {
-		return y
+		return i
 	}
 }
