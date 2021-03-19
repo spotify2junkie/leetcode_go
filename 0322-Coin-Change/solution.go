0f53cbbe6da4757cebcebf862b8b662ab0e0919c
spotify2junkie <lsemarkluo@gmail.com>
2021-03-19 19:12:19 +0800

    add

--- 0322-Coin-Change/solution.go
+++ 0322-Coin-Change/solution.go
@@ -1,26 +1,27 @@
-//nnn dp 
+//nnn dp
 func coinChange(coins []int, amount int) int {
-    dp := make([]int,amount+1)
-    dp[0] = 0 
-    for i:=1;i < len(dp);i++ {
-        dp[i] = amount+100
-    }
-    for i := 1; i <= amount; i++ {
-        for _,coin := range coins {
-            if coin <= i {
-                dp[i] = min(dp[i],dp[i-coin]+1)  // 自己本身（初始的自己（被设定）或者迭代的自己最小）或者 coin的最小值
-            }
-        }
-    }
-    if dp[amount] == amount+100 {
-        return -1
-    } else {
-        return dp[amount]
-    }
+	dp := make([]int, amount+1)
+	for i := 1; i < len(dp); i++ {
+		dp[i] = 1<<31 - 1
+	}
+	for i := 1; i <= amount; i++ {
+		for _, element := range coins {
+			if element <= i {
+				dp[i] = min(dp[i], dp[i-element]+1)
+			}
+		}
+	}
+	if dp[amount] == 1<<31-1 {
+		return -1
+	} else {
+		return dp[amount]
+	}
 }
 
-func min (x,y int) int {
-    if x > y {
-        return y
-    } else { return x}
+func min(i, j int) int {
+	if i < j {
+		return i
+	} else {
+		return j
+	}
 }
